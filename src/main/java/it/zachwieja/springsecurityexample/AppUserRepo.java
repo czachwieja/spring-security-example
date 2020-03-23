fb362abdff9ece73fe65dab11202a875edab6e0e
Cezary Zachwieja <cezary.zachwieja@gmail.com>
2020-03-23 23:34:40 +0100

    model and repo added

---
+++ src/main/java/it/zachwieja/springsecurityexample/AppUserRepo.java
@@ -0,0 +1,8 @@
+package it.zachwieja.springsecurityexample;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepo extends JpaRepository<AppUser, Long> {
+}
